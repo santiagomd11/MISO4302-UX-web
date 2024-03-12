99fed79ca65fc418d8f820577d088a85f06aea66
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-03-12 17:37:46 -0500

    Add view inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstadisticaCalentamientoContenidoComponent } from './estadistica-calentamiento/estadistica-calentamiento-contenido/estadistica-calentamiento-contenido.component';
-import { NavBarComponent } from './menu/nav-bar/nav-bar.component';
+import { InicioSesionContenidoComponent } from './inicio-sesion/inicio-sesion-contenido/inicio-sesion-contenido.component';
 
 const routes: Routes = [
-  { path: '', component: NavBarComponent,  pathMatch: 'full' },
+  { path: '', redirectTo: '/iniciar-sesion', pathMatch: 'full' },
   { path: 'estadistica-calentamiento', component: EstadisticaCalentamientoContenidoComponent,  pathMatch: 'full' },
+  { path: 'iniciar-sesion', component: InicioSesionContenidoComponent,  pathMatch: 'full' },
 ];
 
 @NgModule({
---
+++ src/app/inicio-sesion/inicio-sesion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InicioSesionContenidoComponent } from './inicio-sesion-contenido/inicio-sesion-contenido.component';
+
+
+
+@NgModule({
+  declarations: [
+    InicioSesionContenidoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InicioSesionModule { }
